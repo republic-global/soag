042a4f8663f1ffe61ff957e37a7d49fd3e8606ad
William Pederzoli <wpederzoli@gmail.com>
2024-03-21 14:03:23 +0100

    refactor

--- src/rep/mod.rs
+++ src/rep/mod.rs
@@ -14,64 +14,12 @@ impl Rep {
     }
 
     pub fn separate(&self, target: &PathBuf) {
-        //TODO: This could be abstracted to a prerequisites() fn
-        if let Err(e) = utils::validate_git_repo(&self.directory) {
-            eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
-            return;
-        };
-
-        if let Err(e) = utils::create_rep_directory(&self.directory) {
-            eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
-            return;
-        };
-
-        if let Err(e) = utils::validate_gitignore(&self.directory) {
-            eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
-            return;
-        };
-
-        if let Err(e) = self.add_to_rep(target) {
+        if let Err(e) = self.prerequisites_setup() {
             eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
             return;
         }
 
-        let separated_repo_path = self.directory.join(".rep/").join(target);
-
-        if let Err(e) = git::init(&&separated_repo_path) {
-            eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
-            return;
-        }
-
-        if let Err(e) = git::add_all(&separated_repo_path) {
-            eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
-            return;
-        }
-
-        if let Err(e) = git::commit(
-            &separated_repo_path,
-            format!("{} repository created", target.to_str().unwrap()),
-        ) {
-            eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
-            return;
-        }
-
-        if let Err(e) = git::add_all(&self.directory) {
-            eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
-            return;
-        }
-
-        if let Err(e) = git::commit(
-            &self.directory,
-            format!(
-                "{} separated to its own repository",
-                target.to_str().unwrap()
-            ),
-        ) {
-            eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
-            return;
-        }
-
-        if let Err(e) = git::add_subtree(&self.directory, target.to_str().unwrap()) {
+        if let Err(e) = self.add_new_repo_setup(target) {
             eprintln!("{}{}{}", Fg(Red), e, Fg(termion::color::Reset));
             return;
         }
@@ -94,6 +42,34 @@ impl Rep {
         let new_path = rep_dir.join(path.file_name().unwrap());
 
         fs::rename(path, new_path)?;
+
+        Ok(())
+    }
+
+    fn prerequisites_setup(&self) -> Result<(), std::io::Error> {
+        utils::validate_git_repo(&self.directory)?;
+        utils::create_rep_directory(&self.directory)?;
+        utils::validate_gitignore(&self.directory)?;
+
+        Ok(())
+    }
+
+    fn add_new_repo_setup(&self, repo: &PathBuf) -> Result<(), std::io::Error> {
+        let separated_repo_path = self.directory.join(".rep/").join(repo);
+
+        self.add_to_rep(repo)?;
+        git::init(&separated_repo_path)?;
+        git::add_all(&separated_repo_path)?;
+        git::commit(
+            &separated_repo_path,
+            format!("{} repository created", repo.to_str().unwrap()),
+        )?;
+        git::add_all(&self.directory)?;
+        git::commit(
+            &self.directory,
+            format!("{} separated to its own repository", repo.to_str().unwrap()),
+        )?;
+        git::add_subtree(&self.directory, repo.to_str().unwrap())?;
 
         Ok(())
     }
