3f4044a49150cc65ecb35ef04357cd440e94671e
William Pederzoli <wpederzoli@gmail.com>
2024-04-12 09:34:45 +0200

    Remove termion

--- src/output/output.rs
+++ src/output/output.rs
@@ -1,20 +1,18 @@
 use std::io::{self, Write};
 
-use termion::color::{Blue, Fg, Red, Reset};
-
 //TODO: update outputs to use this mod
 
 pub fn error(error: &str) {
-    eprintln!("{}{}{}", Fg(Red), error, Fg(Reset));
+    eprintln!("{}", error);
 }
 
 pub fn success(msg: &str) {
-    println!("{}{}{}", Fg(Blue), msg, Fg(Reset));
+    println!("{}", msg);
 }
 
 pub fn prompt(msg: &str) -> Result<String, std::io::Error> {
     let mut res = String::new();
-    print!("{}{}{}", Fg(Blue), msg, Fg(Reset));
+    print!("{}", msg);
     io::stdout().flush()?;
 
     if let Err(e) = std::io::stdin().read_line(&mut res) {
